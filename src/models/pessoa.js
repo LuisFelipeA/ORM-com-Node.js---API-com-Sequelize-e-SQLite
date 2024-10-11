cab777171db75ac24a6c2f4d7742643c9d3f3036
LuisFelipeA <felipeadriani2@gmail.com>
2024-10-11 18:56:26 -0300

    Criando modelo Pessoa

---
+++ src/models/pessoa.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    cpf: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+    tableName: 'pessoas'
+  });
+  return Pessoa;
+};

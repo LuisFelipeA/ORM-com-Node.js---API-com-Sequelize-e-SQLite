106ae32bf4475f528bdc94b390dfd9e8f89f21d5
LuisFelipeA <felipeadriani2@gmail.com>
2024-10-15 22:03:32 -0300

    Buscando matriculas do estudante

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -13,5 +13,6 @@ router.get('/pessoas/:id', (req,res) => pessoaController.pegaUmPorId(req, res));
 router.put('/pessoas/:id', (req,res) => pessoaController.atualiza(req, res));
 router.delete('/pessoas/:id', (req,res) => pessoaController.exclui(req, res));
 router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.criaNovo(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req, res));
 
 module.exports = router;

b173cd07538764540d0677b1db02f69bba48fd23
LuisFelipeA <felipeadriani2@gmail.com>
2024-10-17 19:40:22 -0300

    Buscando matriculas ativas e todas as matriculas com rotas separadas

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -14,6 +14,7 @@ router.get('/pessoas/:id', (req,res) => pessoaController.pegaUmPorId(req, res));
 router.put('/pessoas/:id', (req,res) => pessoaController.atualiza(req, res));
 router.delete('/pessoas/:id', (req,res) => pessoaController.exclui(req, res));
 router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.criaNovo(req, res));
-router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculasAtivas(req, res));
+router.get('/pessoas/:estudanteId/matriculas/todas', (req, res) => pessoaController.pegaTodasAsMatriculas(req, res));
 
 module.exports = router;
